3a2dbe184f061ea55bdc66bd03454fc350c9181b
Jason111nn <jason5j288@gmail.com>
2026-01-23 19:21:50 +0800

    Auto deploy update

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,11 @@
-#!/usr/bin/env sh
-# 發生錯誤時終止
-set -e
-# 建置
-npm run build
-# 移動到輸出目錄
-cd dist
-# 部署到 gh-pages 分支
-git init
-git checkout -b main
-git add -A
-git commit -m 'deploy'
-# 記得改成你的 GitHub 網址
-git push -f https://github.com/你的帳號/my-countdown.git main:gh-pages
-cd -
+# 紀念用途
+
+# set -e
+# npm run build
+# cd dist
+# git init
+# git checkout -b main
+# git add -A
+# git commit -m 'deploy'
+# git push -f https://github.com/jason111nn/my-countdown.git main:gh-pages
+# cd -
